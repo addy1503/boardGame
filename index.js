d124b45630e7ef2d7f2082dca6944800cc7ef7a3
aditya <adityashivam98@gmail.com>
2017-06-11 14:10:09 +0530

    timer added

--- index.js
+++ index.js
@@ -2,33 +2,21 @@ var memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H',
 var memory_values = [];
 var memory_tile_ids = [];
 var tiles_flipped = 0;
+function myfunc(){
+	clearTimeout(window.timer);
+	countdown(60,'demo');
+	newBoard();
+}
 
-// Set the date we're counting down to
-function timer(){
-var countDownDate = new Date().getTime()+60000;
-console.log(countDownDate);
-var x = setInterval(function() {
- // Get todays date and time
-    var now = new Date().getTime();
-    
-    // Find the distance between now an the count down date
-    var distance = countDownDate - now;
-    
-    // Time calculations for days, hours, minutes and seconds
-    
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-    // Output the result in an element with id="demo"
-    document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s ";
-    
-    // If the count down is over, write some text 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "Game Over!";
-        document.getElementById('memory_board').innerHTML = "";
-    }
-}, 1000);
+function countdown(secs,demo){
+	var demos=document.getElementById(demo);
+	demos.innerHTML="Time Remaining: "+secs+ ' Seconds.';
+	if (secs<1){
+		clearTimeout(window.timer);
+		demos.innerHTML='Game Over!';
+		}
+		secs--;
+    window.timer = setTimeout('countdown('+secs+',"'+demo+'")',1000);
 }
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
@@ -77,11 +65,17 @@ function memoryFlipTile(tile,val){
 				memory_values = [];
             	memory_tile_ids = [];
 				// Check to see if the whole board is cleared
+				
 				if(tiles_flipped == memory_array.length){
-					alert("Board cleared... generating new board");
+					alert("You win!");
 					document.getElementById('memory_board').innerHTML = "";
-					newBoard();
+					// newBoard();
 				}
+				// else{
+				// 	alert("you lose");
+				// 	newBoard();
+				// }
+				
 			} else {
 				function flip2Back(){
 				    // Flip the 2 tiles back over
@@ -97,21 +91,6 @@ function memoryFlipTile(tile,val){
 				}
 				setTimeout(flip2Back, 700);
 			}
-		}
-	}
+	}		}
+
 }
-function func(){
-	newBoard();
-	timer();
-}
-// function signOut() {
-// 	// gapi.auth2.init({
-//  //      client_id: '894321586184-ern8nufph947ubff8gkkd99hj54lqsl8.apps.googleusercontent.com'
-//  //    });
-//     var auth2 = gapi.auth2.getAuthInstance();
-//     auth2.signOut().then(function () {
-//       console.log('User signed out.');
-//       window.location.replace("index.html");
-//     });
-//   }
-// localstorage.setItem('');
